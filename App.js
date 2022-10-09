3afec22be1ffb7c74f211afe8c3c58e06fd954b9
mcguiretj18 <mcguiretj18@gmail.com>
2022-10-09 00:11:26 -0400

    get infinite scroll working

---
+++ App.js
@@ -0,0 +1,60 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import PokemonList from './components/PokemonList';
+
+const queryClient = new QueryClient();
+
+const App  = () => {
+  const [form, setForm] = useState(null);
+
+  const onChangeText = (name) => (value) => {
+    setForm({ ...form, [name]: value });
+  }
+
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <View style={styles.container}>
+        <Text style={styles.title}>Pokedex</Text>
+        <Text styles={styles.instructions}>Search for a pokemon by name</Text>
+        <TextInput value={form?.pokemonName ?? ""} onChangeText={onChangeText("pokemonName")} />
+        <QueryClientProvider client={queryClient}>
+          <PokemonList pokemonName={form?.pokemonName} />
+        </QueryClientProvider>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10
+  },
+  title: {
+    fontWeight: "bold",
+    marginTop: 10,
+    fontSize: 28
+  },
+  instructions: {
+    marginTop: 8,
+    fontSize: 16
+  }
+});
+
+export default App;
---
+++ components/PokemonList.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Pokemon from './Pokemon';
+
+const BASE_URL = 'https://pokeapi.co/api/v2/pokemon';
+
+const fetchPokemonList = ({ pageParam = 'limit=9&offset=0' }) => {
+    return fetch(`${BASE_URL}?${pageParam}`)
+        .then(response => response.json())
+}
+
+const PokemonList = () => {
+    const { error, data, hasNextPage, fetchNextPage } = useInfiniteQuery(['pokemonList'], fetchPokemonList, {
+        getNextPageParam: lastPage => {
+            if (lastPage.next !== null) {
+                return lastPage.next.split('?')[1];
+            }
+
+            return lastPage;
+        }
+    });
+
+    if (error) {
+        return (
+            <View>
+                <Text>An error has occured.</Text>
+            </View>
+        )
+    }
+
+    const loadMorePokemon = () => {
+        if (hasNextPage) {
+          fetchNextPage();
+        }
+    };
+
+    const renderData = ({ item }) => {
+        return <Pokemon {...item} />
+    }
+
+    const getItemExtractorKey = (_, index) => {
+        return index.toString();
+    }
+
+    return (
+        <View style={styles.container}>
+            <FlatList
+                numColumns={2}
+                horizontal={false}
+                data={data?.pages.map(page => page.results).flat()}
+                keyExtractor={getItemExtractorKey}
+                renderItem={renderData}
+                onEndReached={loadMorePokemon}
+                onEndReachedThreshold={0.3}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center",
+    }
+})
+
+export default PokemonList;
