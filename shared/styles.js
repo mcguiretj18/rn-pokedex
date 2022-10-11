031eb2c74a5fc40f5d88902699b0c5dc2e8b27d9
mcguiretj18 <mcguiretj18@gmail.com>
2022-10-11 04:14:07 -0400

    add bgcolor into shared styles

--- shared/styles.js
+++ shared/styles.js
@@ -23,6 +23,42 @@ const styles = StyleSheet.create({
     },
     alignItemsStart: {
         alignItems: "flex-start"
+    },
+    bug: {
+        backgroundColor: "#D6EEBA"
+    },
+    electric: {
+        backgroundColor: "#EEECBA"
+    },
+    fairy: {
+        backgroundColor: "#EEBAD6"
+    },
+    fighting: {
+        backgroundColor: "#E9C5A6"
+    },
+    fire: {
+        backgroundColor: "#EED2BA"
+    },
+    grass: {
+        backgroundColor: "#C4E2D5"
+    },
+    ground: {
+        backgroundColor: "#E9C5A6"
+    },
+    normal: {
+        backgroundColor: "#E8EBEC"
+    },
+    poison: {
+        backgroundColor: "#D2BAEE"
+    },
+    psychic: {
+        backgroundColor: "#D2BAEE"
+    },
+    rock: {
+        backgroundColor: "#E9C5A6"
+    },
+    water: {
+        backgroundColor: "#BAD6EE"
     }
 });
 
